f2362f72f97872ff43f19ad3b51f59b03125b8f0
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2025-12-31 16:13:06 +0530

    Basic API Test

--- cypress/e2e/api-int-1/getOnly.cy.ts
+++ cypress/e2e/api-int-1/getOnly.cy.ts
@@ -1,9 +1,25 @@
-describe('Users API', () => {
-  it('Verify GET users API call', () => {
+describe('GET API', () => {
+  it('API Test - 1', () => {
     cy.request('https://jsonplaceholder.typicode.com/users')
       .then((response) => {
         expect(response.status).to.eq(200);
         expect(response.body).to.have.length(10);
+      });
+  });
+
+  it('API Test - 2', () => {
+    cy.request('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.length.greaterThan(4);
+      });
+  });
+
+  it('API Test - 3', () => {
+    cy.request('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.length.lessThan(100);
       });
   });
 });
